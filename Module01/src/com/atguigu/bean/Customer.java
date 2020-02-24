72f9743db4ce790aa6075f336ea2cc0ea2a451fa
z0040b8m <shuangshuang.zhang@siemens.com>
2020-02-24 16:12:38 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    public static void main(String[] args){
+        System.out.println("helloworld!!!");
+        ArrayList list = new ArrayList();
+
+    }
+   public void method(){
+
+   }
+
+
+
+}
